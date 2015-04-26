abd95931e9e94c38a351ac3af6e9f92dbceea9af
Fred <f.maquet@gmail.Com>
2015-04-26 21:37:12 +0200

    page html avec properties mixtes entre controller et thymleaf

--- src/main/java/com/soldat/BaseController.java
+++ src/main/java/com/soldat/BaseController.java
@@ -3,7 +3,6 @@ package com.soldat;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,31 +12,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class BaseController {
 
-	private static int counter = 0;
 	private static final String VIEW_INDEX = "index";
-	private final static org.slf4j.Logger logger = LoggerFactory.getLogger(BaseController.class);
-	Locale defaultLocale = Locale.getDefault();
-	ResourceBundle bundle1 = ResourceBundle.getBundle("messages",defaultLocale);
+	private final Locale defaultLocale = Locale.getDefault();
+	private final ResourceBundle MESSAGE = ResourceBundle.getBundle("messages", defaultLocale);
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String welcome(ModelMap model) {
-		String test = bundle1.getString("message.hello");
+		String test = MESSAGE.getString("message.hello.from.controller");
 
 		model.addAttribute("message", test);
-		model.addAttribute("counter", ++counter);
-		logger.debug("[welcome] counter : {}", counter);
 
 		// Spring uses InternalResourceViewResolver and return back index.jsp
 		return VIEW_INDEX;
-
 	}
 
 	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
 	public String welcomeName(@PathVariable String name, ModelMap model) {
-
 		model.addAttribute("message", "" + name);
-		model.addAttribute("counter", ++counter);
-		logger.debug("[welcomeName] counter : {}", counter);
 		return VIEW_INDEX;
 
 	}
