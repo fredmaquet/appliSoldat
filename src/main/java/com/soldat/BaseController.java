861719326959fbd3cd4416592ac4a93e10b7ab7c
Fred <f.maquet@gmail.Com>
2015-04-26 12:20:27 +0200

    Retrait code en erreur pour verification qui le build jenkins passe

--- src/main/java/com/soldat/BaseController.java
+++ src/main/java/com/soldat/BaseController.java
@@ -17,7 +17,7 @@ public class BaseController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String welcome(ModelMap model) {
 
-		model.acfùmlgùdkddAttribute("message", "Test");
+		model.addAttribute("message", "Test");
 		model.addAttribute("counter", ++counter);
 		logger.debug("[welcome] counter : {}", counter);
 
